5d08f89adf5cd6a3a2d136e430dcc86f1287e51d
paulr093 <paulrichan@icloud.com>
2022-02-09 13:26:47 -0700

    added scene

--- components/SidebarTabs/RenderSettings.js
+++ components/SidebarTabs/RenderSettings.js
@@ -2,12 +2,14 @@ import { Disclosure, Switch } from "@headlessui/react"
 import { useRecoilState } from "recoil"
 import { renderSettings } from "../../recoil/states"
 import ContactShadows from "./rendersettings/ContactShadows"
+import Scene from "./rendersettings/Scene"
 
 export default function RenderSettings() {
    return (
       <>
          <div className='flex flex-grow flex-col w-full max-h-screen space-y-2 items-start'>
             <ContactShadows />
+            <Scene />
          </div>
       </>
    )
