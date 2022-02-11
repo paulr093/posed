17e861099cde6982e2b220862408f0222815252b
paulr093 <paulrichan@icloud.com>
2022-02-11 08:35:35 -0700

    styling and link to home

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
 import IconButton from "./shared/IconButton"
-import RenderSettings from "./SidebarTabs/RenderSettings"
-import CharacterSettings from "./SidebarTabs/CharacterSettings"
+import RenderSettings from "./sidebartabs/RenderSettings"
+import CharacterSettings from "./sidebartabs/CharacterSettings"
 import { PhotographIcon, CameraIcon, CogIcon } from "@heroicons/react/outline"
-import {saveAs} from "file-saver"
+import { saveAs } from "file-saver"
 import { characterState } from "../recoil/states"
 import { useRecoilValue } from "recoil"
+import Link from "next/link"
 
 function Sidebar() {
    const [tab, setTab] = useState({
@@ -13,18 +14,20 @@ function Sidebar() {
       character: false,
       settings: false,
    })
-   const {label: label} = useRecoilValue(characterState)
+   const { label: label } = useRecoilValue(characterState)
 
    function Screenshot() {
       const canvas = document.getElementsByTagName("canvas")[0].toDataURL("image/png")
-      const downloadTitle = label.replace(/\s/g, '') + ".png"
+      const downloadTitle = label.replace(/\s/g, "") + ".png"
 
       saveAs(canvas, downloadTitle)
    }
 
    return (
-      <div className='flex flex-grow flex-col h-full space-y-4 items-center p-3 font-montserrat font-medium'>
-         <h1 className='font-bold text-3xl font-gugi'>Posed</h1>
+      <div className='flex flex-grow flex-col h-full space-y-4 items-center p-3 font-montserrat font-medium dark:text-white'>
+         <Link href='/' passHref>
+            <h1 className='font-bold text-3xl font-gugi cursor-pointer select-none hover:bg-neutral-200 p-2 rounded-lg duration-150'>Posed</h1>
+         </Link>
          <div className='flex flex-row justify-evenly w-full'>
             <IconButton
                active={tab.render}
@@ -48,7 +51,7 @@ function Sidebar() {
 
          <button
             onClick={() => Screenshot()}
-            className='bg-slate-200 p-2 rounded-md hover:bg-blue-500 hover:text-white duration-150'
+            className='p-3 rounded-full font-bold bg-gradient-to-r from-blue-500 to-emerald-500 text-white hover:-translate-x-1 hover:-translate-y-1 duration-150 hover:shadow-md'
          >
             Screenshot
          </button>
