06548361b2d6be98bd82e78e3593fd6a51aaa79d
paulr093 <paulrichan@icloud.com>
2022-02-11 14:34:02 -0700

    global Head

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document {
       return (
          <Html>
             <Head>
+               <meta name='description' content='Snap photos of fully customizable 3d models!' />
+               <link rel='icon' href='/favicon.ico' />
+
                <link rel='preconnect' href='https://fonts.googleapis.com' />
                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
                <link href='https://fonts.googleapis.com/css2?family=Gugi&display=swap' rel='stylesheet' />
