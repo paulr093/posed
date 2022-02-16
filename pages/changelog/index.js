3c597973ac72569739c16a8ca7d8228852537f5b
paulr093 <paulrichan@icloud.com>
2022-02-16 16:00:05 -0700

    changelog v0.0.4

--- pages/changelog/index.js
+++ pages/changelog/index.js
@@ -16,7 +16,9 @@ function Home() {
                   <h2 className='font-semibold text-xl'>v0.0.4</h2>
                   <h3 className='text-sm opacity-50'>February 16th, 2022</h3>
                   <p className='px-5'>
-                     You can now change the roughness and metalness on every color, adding another layer of customization.
+                     You can now change the roughness and metalness on every color, adding another layer of
+                     customization. If you want to create an account with us you are now able to! There is not much you
+                     can do with your account right now but we have big plans for the future.
                   </p>
                </div>
 
@@ -27,7 +29,7 @@ function Home() {
                      Introducing the changelog! We handled some mobile and dark mode styles. Added a brand new, full
                      transformable
                      <Link href='/viewer/Low Poly Spaceman' passHref>
-                        <span className='text-blue-500 hover:text-blue-700 cursor-pointer'>{" "}Low Poly Spaceman</span>
+                        <span className='text-blue-500 hover:text-blue-700 cursor-pointer'> Low Poly Spaceman</span>
                      </Link>
                      ! We have also introduced Dynamic Routing so that you can share with your friends the link to a
                      specific model.
